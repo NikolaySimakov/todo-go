557eb15451cba6214d20628fd7401e938644570a
Nikolay Simakov <n.simakoff@gmail.com>
2023-07-14 22:54:15 +0300

    Import controllers

--- routes/web.go
+++ routes/web.go
@@ -1,14 +1,16 @@
 package routes
 
-import "github.com/go-chi/chi"
+import (
+	"github.com/NikolaySimakov/todo-go/controllers"
+
+	"github.com/gorilla/mux"
+)
 
 func Init() *mux.Router {
 	route := mux.NewRouter()
-
 	route.HandleFunc("/", controllers.Show)
 	route.HandleFunc("/add", controllers.Add).Methods("POST")
 	route.HandleFunc("/delete/{id}", controllers.Delete)
 	route.HandleFunc("/complete/{id}", controllers.Complete)
-
 	return route
 }
