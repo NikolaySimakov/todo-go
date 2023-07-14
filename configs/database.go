7b5b25f5d6c9708234aed0a9d42855baf3669f2e
Nikolay Simakov <n.simakoff@gmail.com>
2023-07-14 22:23:36 +0300

    Database config

--- configs/database.go
+++ configs/database.go
@@ -1,1 +1,30 @@
 package configs
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "314159263"
+	dbname   = "tododb"
+)
+
+func database() *sql.DB {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+	db, err := sql.Open("postgres", psqlInfo)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return db
+}
