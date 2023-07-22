2c01dbc497c401d280faac75f67582a54eacf3ec
Nikolay Simakov <n.simakoff@gmail.com>
2023-07-22 14:53:21 +0300

    Update methods like in basic/todo

--- controllers/stack/todo.go
+++ controllers/stack/todo.go
@@ -17,70 +17,40 @@ var (
 	db        = configs.Database()
 )
 
-// show tasks
-func Show(w http.ResponseWriter, r *http.Request) {
-	rows, err := db.Query("SELECT * FROM tasks")
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	defer rows.Close()
-
-	// creates tasks list
-	var tasks models.List
-
-	// run rows gererator
-	for rows.Next() {
-		err := rows.Scan(&id, &title, &completed)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		tasks = append(tasks, models.Task{
-			Id:         id,
-			Title:      title,
-			IsComplete: completed,
-		})
-	}
-
-	fmt.Println(tasks)
-}
-
 // show current (last) task
 func Current(w http.ResponseWriter, r *http.Request) {
-	row := db.QueryRow("SELECT * FROM tasks", 1)
+
+	// select last task
+	row := db.QueryRow("SELECT * FROM tasks ORDER BY id DESC")
+
 	switch err := row.Scan(&id, &title, &completed); err {
 	case sql.ErrNoRows:
 		fmt.Println("No rows were returned!")
 	case nil:
-		fmt.Println(id)
+		task := models.Task{
+			Id:         id,
+			Title:      title,
+			IsComplete: completed,
+		}
+
+		fmt.Println(task)
 	default:
 		log.Fatal(err)
 	}
-
-	task := models.Task{
-		Id:         id,
-		Title:      title,
-		IsComplete: completed,
-	}
-
-	fmt.Println(task)
 }
 
 // push task to stack
 func Add(w http.ResponseWriter, r *http.Request) {
-	// test task
-	task := models.Task{
-		Title:      "qwerty",
-		IsComplete: false,
-	}
 
-	_, err := db.Exec("INSERT INTO tasks (title, is_completed) VALUES ($1, $2)", task.Title, task.IsComplete)
-	if err != nil {
+	// getting request body
+	title = r.FormValue("title")
+
+	if _, err := db.Exec("INSERT INTO tasks (title, is_completed) VALUES ($1, false)", title); err != nil {
+		// TODO: http.Redirect(w, r, "/", 409)
 		log.Fatal(err)
 	}
-	fmt.Println("Successfully inserted!")
+
+	http.Redirect(w, r, "/", 301)
 }
 
 // pop task from stack
@@ -89,12 +59,6 @@ func Complete(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		log.Fatal(err)
 	}
-}
 
-// cleat stack
-func Delete(w http.ResponseWriter, r *http.Request) {
-	_, err := db.Exec("DELETE FROM tasks")
-	if err != nil {
-		log.Fatal(err)
-	}
+	http.Redirect(w, r, "/", 301)
 }
