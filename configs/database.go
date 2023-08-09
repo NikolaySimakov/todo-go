a55de433cd3aca4d0b62ab629bb80c036beb804e
Nikolay Simakov <n.simakoff@gmail.com>
2023-08-09 22:57:17 +0300

    Update config: use env

--- configs/database.go
+++ configs/database.go
@@ -4,23 +4,40 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"os"
 
 	_ "github.com/lib/pq"
 )
 
-const (
-	host     = "localhost"
-	port     = 5432
-	user     = "postgres"
-	password = "314159263"
-	dbname   = "tododb"
-)
+type DataBaseInfo struct {
+	Host     string
+	Port     string
+	User     string
+	Password string
+	Name     string
+}
+
+func (info *DataBaseInfo) GetEnv() {
+	info.Host = os.Getenv("DB_HOST")
+	info.Port = os.Getenv("DB_PORT")
+	info.User = os.Getenv("DB_USER")
+	info.Password = os.Getenv("DB_PASSWORD")
+	info.Name = os.Getenv("DB_NAME")
+}
+
+func (info *DataBaseInfo) ToString() string {
+	return fmt.Sprintf("host=%s port=%s user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		info.Host, info.Port, info.User, info.Password, info.Name)
+}
 
 func Database() *sql.DB {
-	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
-		"password=%s dbname=%s sslmode=disable",
-		host, port, user, password, dbname)
-	db, err := sql.Open("postgres", psqlInfo)
+	// get connection info
+	connInfo := DataBaseInfo{}
+	connInfo.GetEnv()
+
+	// open DataBase
+	db, err := sql.Open("postgres", connInfo.ToString())
 
 	if err != nil {
 		log.Fatal(err)
